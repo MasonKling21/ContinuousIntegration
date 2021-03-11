70005f361427e11f1dc00ee782beb113f662036a
Mason Kling <klingma@oregonstate.edu>
2021-03-11 09:33:08 -0800

    Fixed issues

--- test_calculator.py
+++ test_calculator.py
@@ -2,9 +2,10 @@ import calculator
 
 
 class TestCalculator:
+
+    
     def test_addition(self):
         assert 5 == calculator.addition(1, 4)
-
 
     def test_subtraction(self):
         assert 2 == calculator.subtraction(5, 3)
